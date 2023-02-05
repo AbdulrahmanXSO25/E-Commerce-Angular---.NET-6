b225d0e50579c725b0e812a936710584e8601370
Abdulrahman <abdulrahmanxso25@outlook.com>
2023-02-05 09:26:59 +0200

    add lazy loading

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home/home.component';
-import { ProductDetailsComponent } from './shop/product-details/product-details.component';
-import { ShopComponent } from './shop/shop.component';
 
 const routes: Routes = [
   {path: '', component:HomeComponent},
-  {path: 'shop', component:ShopComponent},
-  {path: 'shop/:id', component:ProductDetailsComponent},
+  {path: 'shop', loadChildren: ()=> import('./shop/shop.module').then(m => m.ShopModule)},
   {path: '**', redirectTo: '', pathMatch:'full'}
 ];
 
--- client/src/app/shop/shop-routing.module.ts
+++ client/src/app/shop/shop-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+
+const routes: Routes = [
+  {path: '', component:ShopComponent},
+  {path: ':id', component:ProductDetailsComponent}
+];
+
+
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ],
-  exports: []
+  exports: [RouterModule]
 })
 export class ShopRoutingModule { }
