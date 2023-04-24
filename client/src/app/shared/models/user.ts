a7d365a83c6a111ba15e976491a181eba73bd13c
Abdulrahman <abdulrahmanxso25@outlook.com>
2023-04-24 02:12:04 +0200

    crerating the user and address models

---
+++ client/src/app/shared/models/user.ts
@@ -0,0 +1,15 @@
+export interface IUser {
+  email: string;
+  displayName: string;
+  token: string;
+}
+
+export interface IAddress {
+  firstName: string;
+  lastName: string;
+  street: string;
+  city: string;
+  zipCode: string;
+}
+
+
