513f2dc1c457718b877f7b7aa55f5eea9def26fe
Abdulrahman <abdulrahmanxso25@outlook.com>
2023-04-29 10:03:03 +0300

    using replay subject[instead of subject behavior]

--- client/src/app/account/account.service.ts
+++ client/src/app/account/account.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { BehaviorSubject, map } from 'rxjs';
+import { BehaviorSubject, map, of, ReplaySubject } from 'rxjs';
 import { environment } from 'src/env/environment';
 import { IUser } from '../shared/models/user';
 
@@ -12,19 +12,29 @@ export class AccountService {
 
   baseUrl = environment.apiUrl;
 
-  private currentUserSource = new BehaviorSubject<IUser | null>(null);
+  private currentUserSource = new ReplaySubject<IUser | null>(1);
   currentUser$ = this.currentUserSource.asObservable();
 
   constructor(private http:HttpClient, private router: Router) { }
 
-  loadCurrentUser(token: string) {
+  loadCurrentUser(token: string | null) {
+    if (token === null) {
+      this.currentUserSource.next(null);
+      return of(null);
+    }
+
     let headers = new HttpHeaders();
     headers = headers.set('Authorization', `Bearer ${token}`);
 
     return this.http.get<IUser>(this.baseUrl + 'account', {headers}).pipe(
       map(user => {
-        localStorage.setItem('token', user.token);
-        this.currentUserSource.next(user);
+        if (user) {
+          localStorage.setItem('token', user.token);
+          this.currentUserSource.next(user);
+        }
+        else {
+          return null;
+        }
       })
     )
   }
